87208040c1706b5c9c2f68ddd754aef84093305f
L1chik <55959772+L1chik@users.noreply.github.com>
2019-12-30 03:57:32 +0300

    Delete lab_1.m

--- lab_1.m
+++
@@ -1,7 +0,0 @@
-a = 1.3; b = 2.42; c = 0.83; x = 1.5; k = 2;
-y = (abs(pow2(a,2)-pow2(b,2))/sin(k*x))+pow2(10,4) * pow2(abs(sin(k*x)-b*c),0.2)-(pow2(k,2) + tan(3*k))/exp(k*x);
-vector = [ a b c x k y];
-vector_sort = sort(vector);
-sra_vector_sort = mean(vector_sort);
-vector(3) = vector(3) + sra_vector_sort;
-
